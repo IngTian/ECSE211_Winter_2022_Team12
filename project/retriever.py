a18fd320b0c0690d48918865595397055ea4311f
Aidan Jackson <aidan.jackson@mail.mcgill.ca>
2022-03-13 18:24:05 -0400

    Added initial software motor command

--- project/retriever.py
+++ project/retriever.py
@@ -6,16 +6,31 @@ Retrieve foam cubes given a certain color.
 """
 
 # Adjust these imports based on your implementation
-from logic import get_bin_for_color
-from utils.brick import Color, configure_ports
+#from logic import get_bin_for_color
+from utils.brick import Motor
+import time
 
 
 # Initialize hardware devices here 
+m = Motor("A")
+m.reset_encoder()
+m.set_limits(power=80)
+m.set_limits(dps=360)
+
+# limits are used for position movement, not power movement
+def main():
+    m.set_position(-300)
+    while(input("here") != "bye"):
+        pass
+    m.set_position(0)
+    time.sleep(2)
+    
+        
 
 
 # Define your classes and functions here. Consider using other files if this one gets too large
-
-
+    
 
 if __name__ == "__main__":
     print("Starting shoe retrieval system...")
+    main()
--- sample_code/_sample_motors.py
+++ sample_code/_sample_motors.py
@@ -19,26 +19,26 @@ January 24th, 2022
 
 from utils.brick import Motor
 
-motor_left = Motor("A")
+theMotor = Motor("A")
 
-motor_left.set_power(50)
-print("motor_left.set_power(50)")
+theMotor.set_power(50)
+print("theMotor.set_power(50)")
 input("# Press any key to continue...")
 
 # limits are used for position movement, not power movement
-motor_left.set_limits(power=20)
+theMotor.set_limits(power=20)
 
-motor_left.set_power(70) # increase to 70% instead of 20%
-print("motor_left.set_power(70)")
+theMotor.set_power(70) # increase to 70% instead of 20%
+print("theMotor.set_power(70)")
 input("# Press any key to continue...")
 
-motor_left.set_power(0) # always do 0% to stop motor
-print("motor_left.set_power(0)")
+theMotor.set_power(0) # always do 0% to stop motor
+print("theMotor.set_power(0)")
 input("# Press any key to continue...")
 
 # float_motor lets it move freely. 0% power resists movement.
-motor_left.float_motor()
-print("motor_left.float_motor()")
+theMotor.float_motor()
+print("theMotor.float_motor()")
 input("# Press any key to continue...")
 
 ##############################
@@ -48,23 +48,23 @@ input("# Press any key to continue...")
 from utils.brick import Motor
 import time
 
-motor_left = Motor("A")
+theMotor = Motor("A")
 
 # Set target speed first, 360 deg/sec
 # Reset power limit to limitless with 0, default values:(power=0, dps=0)
-motor_left.set_limits(dps=360)
+theMotor.set_limits(dps=360)
 
 # set current position to absolute pos 0deg
-motor_left.reset_encoder()
+theMotor.reset_encoder()
 
 # command to move to absolute pos 270deg
-motor_left.set_position(270)
-print("motor_left.set_position(270)")
+theMotor.set_position(270)
+print("theMotor.set_position(270)")
 input("# Press any key to continue...")
 
 # command to rotate 90deg away from current position
-motor_left.set_position_relative(90)
-print("motor_left.set_position_relative(90)")
+theMotor.set_position_relative(90)
+print("theMotor.set_position_relative(90)")
 input("Press any key to continue...")
 
 """Tests 3 different speeds. set_dps overrides set_limits.
@@ -73,9 +73,9 @@ dps=360, rotation_dist=1080
 dps=540, rotation_dist=1440
 """
 for i, speed in enumerate([180, 360, 180*3]):
-    motor_left.set_dps(speed) # overrides previous limits. Use limits or set_dps
-    motor_left.set_position_relative(360 * (i+2))
-    motor_left.wait_is_moving()
-    while motor_left.is_moving():
+    theMotor.set_dps(speed) # overrides previous limits. Use limits or set_dps
+    theMotor.set_position_relative(360 * (i+2))
+    theMotor.wait_is_moving()
+    while theMotor.is_moving():
         time.sleep(0.1)
-        print("actual speed=", motor_left.get_dps(), "actual power=", motor_left.get_power(), "status=", motor_left.get_status())
+        print("actual speed=", theMotor.get_dps(), "actual power=", theMotor.get_power(), "status=", theMotor.get_status())
